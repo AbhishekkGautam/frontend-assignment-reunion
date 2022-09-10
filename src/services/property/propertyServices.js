ca36939ca40ffbf97686db45c50b705ad907bad4
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2022-09-10 14:47:32 +0530

    fix: api url

--- src/services/property/propertyServices.js
+++ src/services/property/propertyServices.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-export const getPropertyDataService = () =>
-  axios.get("https://621efe11849220b1fca6c22a.mockapi.io/property");
+const API_URL = "https://621efe11849220b1fca6c22a.mockapi.io/property";
+
+export const getPropertyDataService = () => axios.get(API_URL);
